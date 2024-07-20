08320249e037e30fcb9e2756f83892d2805c284e
Davide Bronzini <davide_bronzini@hotmail.com>
2024-07-20 11:07:52 +0200

    all

--- resources/lang/it/fields.php
+++ resources/lang/it/fields.php
@@ -1,10 +1,12 @@
 <?php
 
 return [
-	'name' => 'Nome',
-    'contactable' => 'Contatto',
-    'contacttype' => 'Tipologia',
-    'sorting_index' => 'Ordinamento',
-    'contact' => 'Contatto',
-    'contacts_count' => 'Contatti'
+  'name' => 'Nome',
+  'contactable' => 'Contatto',
+  'contacttype' => 'Tipologia',
+  'sorting_index' => 'Ordinamento',
+  'contact' => 'Contatto',
+  'contacts_count' => 'Contatti',
+  'mySelfTargetType.contactable' => 'Elemento',
+  'created_at' => 'Creato il',
 ];
